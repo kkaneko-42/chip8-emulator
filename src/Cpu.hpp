1e524b210086198f43c71b796ad4a03abf88db7d
kkaneko <gorutiru@gmail.com>
2023-02-12 02:27:59 +0900

    define RAM interface

--- src/Cpu.hpp
+++ src/Cpu.hpp
@@ -10,6 +10,7 @@ namespace chip8 {
                 // 一般的な用途のレジスタ
                 unsigned char v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, va, vb, vc, vd, ve, vf;
                 // メモリアドレスを格納するレジスタ
+                // NOTE: 通常、下位12bitのみ使われる
                 uint16_t i;
                 // タイマー
                 // 0以外の値の時、60Hzで値がデクリメントされる
@@ -20,6 +21,15 @@ namespace chip8 {
                 unsigned char sp;
                 // NOTE: STACK_SIZEは、入れ子にできるルーチンの数(深さ)
                 uint16_t stack[STACK_SIZE];
+            };
+
+            class IRandomAccessMemory {
+                public:
+                    virtual ~IRamdomAccessMemory() {};
+
+                    virtual unsigned char* load(uint16_t addr, size_t n) = 0;
+                    virtual void store(uint16_t addr, unsigned char* data, size_t n) = 0;
+                    virtual size_t getCapacity() = 0;
             };
     };
 }
