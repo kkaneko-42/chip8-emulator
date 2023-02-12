1c8e23ee9674ee7913c06950fb9c1609d0a58342
kkaneko <gorutiru@gmail.com>
2023-02-12 17:05:01 +0900

    fix: registers's data type

--- src/Cpu.hpp
+++ src/Cpu.hpp
@@ -9,17 +9,17 @@ namespace chip8 {
         public:
             struct Registers {
                 // 一般的な用途のレジスタ
-                unsigned char v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, va, vb, vc, vd, ve, vf;
+                uint8_t v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, va, vb, vc, vd, ve, vf;
                 // メモリアドレスを格納するレジスタ
                 // NOTE: 通常、下位12bitのみ使われる
                 uint16_t i;
                 // タイマー
                 // 0以外の値の時、60Hzで値がデクリメントされる
-                unsigned char delay_timer, sound_timer;
+                uint8_t delay_timer, sound_timer;
                 // program counter
                 uint16_t pc;
                 // stack pointer
-                unsigned char sp;
+                uint8_t sp;
                 // NOTE: STACK_SIZEは、入れ子にできるルーチンの数(深さ)
                 uint16_t stack[STACK_SIZE];
             };
