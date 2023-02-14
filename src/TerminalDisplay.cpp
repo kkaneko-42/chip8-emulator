1d343045cba6e1664c4af4d6c8554c3d942d5a75
kkaneko <gorutiru@gmail.com>
2023-02-14 18:14:17 +0900

    implement TerminalDisplay::renderSprite(), but not tested

--- src/TerminalDisplay.cpp
+++ src/TerminalDisplay.cpp
@@ -2,8 +2,8 @@
 
 using namespace chip8;
 
-const size_t TerminalDisplay::kResolutionX = 64;
-const size_t TerminalDisplay::kResolutionY = 32;
+const size_t TerminalDisplay::kWidth = 64;
+const size_t TerminalDisplay::kHeight = 32;
 
 TerminalDisplay::TerminalDisplay() {
     initscr();
@@ -14,7 +14,17 @@ TerminalDisplay::~TerminalDisplay() {
 }
 
 bool TerminalDisplay::renderSprite(size_t x, size_t y, const std::vector<unsigned char>& sprite) {
-    return false;
+    bool collision = false;
+
+    for (const auto& sprite_row : sprite) {
+        if ((frame_buffer_[y] & sprite_row) == 0) {
+            collision = true;
+        }
+        frame_buffer_[y] ^= static_cast<uint64_t>(sprite_row) >> x;
+        ++y;
+    }
+
+    return collision;
 }
 
 void TerminalDisplay::clear() {
--- src/TerminalDisplay.hpp
+++ src/TerminalDisplay.hpp
@@ -2,6 +2,7 @@
 
 #include "Cpu.hpp"
 #include <ncurses.h>
+#define WIDTH 64
 
 namespace chip8 {
     class TerminalDisplay : public Cpu::IDisplay {
@@ -10,9 +11,11 @@ namespace chip8 {
             ~TerminalDisplay();
             bool renderSprite(size_t x, size_t y, const std::vector<unsigned char>& sprite) override;
             void clear() override;
-            void getResolution(size_t& x, size_t& y) override { x = kResolutionX; y = kResolutionY; };
+            void getResolution(size_t& x, size_t& y) override { x = kWidth; y = kHeight; };
 
         private:
-            static const size_t kResolutionX, kResolutionY;
+            static const size_t kWidth, kHeight;
+
+            uint64_t frame_buffer_[kHeight];
     };
 }
