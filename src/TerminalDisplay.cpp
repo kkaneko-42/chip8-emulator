c6dbfafd0a7b1942f6a102c98458abe7a2d453cd
kkaneko <gorutiru@gmail.com>
2023-02-14 17:23:12 +0900

    create TerminalDisplay::create(), but not implemented

--- src/TerminalDisplay.cpp
+++ src/TerminalDisplay.cpp
@@ -3,6 +3,10 @@
 const size_t TerminalDisplay::kResolutionX = 64;
 const size_t TerminalDisplay::kResolutionY = 32;
 
-bool Display::renderSprite(size_t x, size_t y, const std::vector<unsigned char>& sprite) {
+bool TerminalDisplay::renderSprite(size_t x, size_t y, const std::vector<unsigned char>& sprite) {
 
+}
+
+void TerminalDisplay::clear() {
+    
 }
--- src/TerminalDisplay.hpp
+++ src/TerminalDisplay.hpp
@@ -7,6 +7,7 @@ namespace chip8 {
     class TerminalDisplay : public Cpu::IDisplay {
         public:
             bool renderSprite(size_t x, size_t y, const std::vector<unsigned char>& sprite) override;
+            void clear() override;
             void getResolution(size_t& x, size_t& y) override { x = kResolutionX; y = kResolutionY; };
 
         private:
