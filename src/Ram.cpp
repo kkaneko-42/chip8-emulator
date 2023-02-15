86c3d14e5ea97bda9bcb0687b5d465d529b776a4
kkaneko <gorutiru@gmail.com>
2023-02-15 12:53:47 +0900

    implement ram

--- src/Ram.cpp
+++ src/Ram.cpp
@@ -2,14 +2,22 @@
 
 using namespace chip8;
 
-std::vector<unsigned char> Ram::load(uint16_t addr, size_t n) {
+const size_t Ram::kCapacity = 4096;
 
+Ram::Ram() {
+    data_.resize(kCapacity);
+}
+
+std::vector<unsigned char> Ram::load(uint16_t addr, size_t n) {
+    return {data_.begin() + addr, data_.begin() + addr + n};
 }
 
 void Ram::store(uint16_t addr, const std::vector<unsigned char>& data) {
-
+    for (size_t i = 0; i < data.size(); ++i) {
+        data_[i + addr] = data[i];
+    }
 }
 
 size_t Ram::getCapacity() {
-
+    return data_.size();
 }
--- src/Ram.hpp
+++ src/Ram.hpp
@@ -5,8 +5,13 @@
 namespace chip8 {
     class Ram : public Cpu::IRandomAccessMemory {
         public:
+            Ram();
             std::vector<unsigned char> load(uint16_t addr, size_t n) override;
             void store(uint16_t addr, const std::vector<unsigned char>& data) override;
             size_t getCapacity() override;
+
+        private:
+            static const size_t kCapacity;
+            std::vector<unsigned char> data_;
     };
 }
